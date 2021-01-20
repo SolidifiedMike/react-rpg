ea09f8fa0aa27cbe9200c987a90f54ca796a5213
mikews <mikews_wwzl@icloud.com>
2021-01-20 08:38:29 -0600

    added window hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import World from "./components/world";
+import useWindowDimensions from "./hooks/use-window-dimensions";
 
 function App() {
+  const { width, height } = useWindowDimensions();
+  console.log("width: " + width + " hieght: " + height);
+
   return (
-    <div>
+    <div style={{
+      width,
+      height,
+      backgroundColor: "grey"
+      }}>
       <World />
     </div>
   );
